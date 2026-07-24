df6e07189c0c761af5313ec5e71b3a891e17f1ce
Erdaulet <124573488+talasbekov@users.noreply.github.com>
2026-07-24 10:43:39 +0500

    feat(storage): export photo reference guard

--- apps/api/src/storage/storage.module.ts
+++ apps/api/src/storage/storage.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { FILE_STORAGE } from './storage.interface';
 import { LocalDiskStorage } from './local-disk.storage';
+import { PhotoReferenceGuard } from './photo-reference.guard';
 
 @Module({
-  providers: [{ provide: FILE_STORAGE, useClass: LocalDiskStorage }],
-  exports: [FILE_STORAGE],
+  providers: [{ provide: FILE_STORAGE, useClass: LocalDiskStorage }, PhotoReferenceGuard],
+  exports: [FILE_STORAGE, PhotoReferenceGuard],
 })
 export class StorageModule {}
